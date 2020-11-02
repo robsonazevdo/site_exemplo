f86f47185c2527946080ae82ef2f65b583207db7
robsonazevedo <kassapete@hotmail.com>
2020-11-02 18:47:49 -0300

    termino do menu e começo de login

--- static/js/menu.js
+++ static/js/menu.js
@@ -16,11 +16,44 @@ menuDoc.addEventListener("click", function() {
  */
 
 var iconMenu = document.querySelector(".icon-menu");
-var menu = document.querySelector(".menu");
+//var menu = document.querySelector(".menu");
 var conteudo = document.querySelector(".content");
 
+let body = document.querySelector("body");
+
+
 iconMenu.addEventListener("click", function() {
-    menu.classList.toggle("menu__move");
-    conteudo.classList.toggle("content-move");
+
+    body.classList.toggle("__move");
+    //menu.classList.toggle("menu__move");
+    //conteudo.classList.toggle("content-move");
+
+});
+/*recolhimento*/
+
+
+conteudo.addEventListener("click", function() {
+
+    body.classList.remove("__move");
+});
+
+
+/*
+MENU ATIVO
+*/
+
+let linkNavs = document.querySelectorAll(".link-nav");
+
+linkNavs.forEach(function(valorcorrente, index, linkNavs) {
+
+    valorcorrente.addEventListener("click", function() {
+
+        linkNavs.forEach(function(element) {
+            element.classList.remove("active");
+        });
+
+        valorcorrente.classList.add("active");
+
+    });
 
 });
